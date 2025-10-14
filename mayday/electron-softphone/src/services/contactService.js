5edea8d88188a08e327649d5372e997ecce404e1
Dlu6 <medhi.mattovu@gmail.com>
2025-10-14 16:13:11 +0300

    Update API_BASE_URL configuration for development and production environments in contactService.js

--- mayday/electron-softphone/src/services/contactService.js
+++ mayday/electron-softphone/src/services/contactService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { storageService } from "./storageService";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
-// || "http://localhost:8004/api";
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8004/api"
+    : "https://cs.hugamara.com/mayday-api";
 
 class ContactService {
   constructor() {
